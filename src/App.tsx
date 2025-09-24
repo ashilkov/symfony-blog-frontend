8ef85a3d34b793ba239a181619b528a45ca7a249
Andrei Shilkov <mazal756@gmail.com>
2025-09-24 18:20:56 +0200

    Add mantine packages for a better editor experience. Rewrite tiptap editor component

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,32 @@ import Posts from "./pages/Posts";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
 import About from "./pages/About";
+import { MantineProvider } from "@mantine/core";
 
 // removed placeholder Posts component in favor of page
 const Contact = () => <div>Contact</div>;
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Posts />} />
-          <Route path="/post/:postId" element={<Post />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/blog/:blogId" element={<Blog />} />
-          <Route path="/posts/new" element={<CreatePost />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/blogs/new" element={<CreateBlog />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <MantineProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Posts />} />
+            <Route path="/post/:postId" element={<Post />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/blog/:blogId" element={<Blog />} />
+            <Route path="/posts/new" element={<CreatePost />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/blogs/new" element={<CreateBlog />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </MantineProvider>
   );
 }
 
--- src/components/TiptapEditor.tsx
+++ src/components/TiptapEditor.tsx
@@ -1,14 +1,19 @@
 import "../assets/styles/tiptap.css";
+import "tiptap-extension-resizable-image/styles.css";
+
+import "@mantine/core/styles.css";
+import "@mantine/tiptap/styles.css";
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Highlight from "@tiptap/extension-highlight";
 import Typography from "@tiptap/extension-typography";
-import Link from "@tiptap/extension-link";
-import Image from "@tiptap/extension-image";
+// import Link from "@tiptap/extension-link";
+import TextAlign from "@tiptap/extension-text-align";
 import { Placeholder } from "@tiptap/extensions";
-import FileHandler from "@tiptap/extension-file-handler";
 import { uploadImage } from "../lib/file";
+import { ResizableImage } from "tiptap-extension-resizable-image";
+import { Link, RichTextEditor } from "@mantine/tiptap";
 
 export function TiptapEditor({ content = "", name = "", onChange }) {
   const editor = useEditor({
@@ -18,59 +23,25 @@ export function TiptapEditor({ content = "", name = "", onChange }) {
       StarterKit,
       Highlight,
       Link,
-      Image,
       Typography,
+      TextAlign.configure({ types: ["heading", "paragraph"] }),
       Placeholder.configure({ placeholder: "Write something ..." }),
-      FileHandler.configure({
-        allowedMimeTypes: [
-          "image/png",
-          "image/jpeg",
-          "image/gif",
-          "image/webp",
-        ],
-        onDrop: async (currentEditor, files, pos) => {
-          for (let file of files) {
-            try {
-              const url = await uploadImage({ file: file });
-              currentEditor
-                .chain()
-                .insertContentAt(pos, {
-                  type: "image",
-                  attrs: {
-                    src: url,
-                    style: "max-width:100%; max-height:400px;",
-                  },
-                })
-                .focus()
-                .run();
-            } catch (error) {
-              console.error("Error uploading file:", error);
-            }
+      ResizableImage.configure({
+        defaultWidth: 200,
+        defaultHeight: 200,
+        async onUpload(file: File) {
+          let src = URL.createObjectURL(file);
+          console.log(src);
+          try {
+            src = await uploadImage({ file: file });
+          } catch (error) {
+            console.error("Error uploading file:", error);
           }
-        },
-        onPaste: async (currentEditor, files, htmlContent) => {
-          for (let file of files) {
-            if (htmlContent) {
-              console.log(htmlContent);
-              return false;
-            }
-            try {
-              const url = await uploadImage({ file: file });
-              currentEditor
-                .chain()
-                .insertContentAt(currentEditor.state.selection.anchor, {
-                  type: "image",
-                  attrs: {
-                    src: url,
-                    style: "max-width:100%; max-height:400px;",
-                  },
-                })
-                .focus()
-                .run();
-            } catch (error) {
-              console.error("Error uploading file:", error);
-            }
-          }
+          console.log(src);
+          return {
+            src,
+            "data-keep-ratio": true,
+          };
         },
       }),
     ],
@@ -80,11 +51,60 @@ export function TiptapEditor({ content = "", name = "", onChange }) {
     editorProps: {
       attributes: {
         name: name,
-        class: "tiptap-editor",
+        // class: "tiptap-editor",
       },
     },
+    immediatelyRender: false,
   });
 
+  return (
+    <RichTextEditor
+      editor={editor}
+      classNames={{
+        content: "content",
+        toolbar: "toolbar",
+      }}
+    >
+      <RichTextEditor.Toolbar sticky stickyOffset={60}>
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Bold />
+          <RichTextEditor.Italic />
+          <RichTextEditor.Strikethrough />
+          <RichTextEditor.ClearFormatting />
+          <RichTextEditor.Code />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.H1 />
+          <RichTextEditor.H2 />
+          <RichTextEditor.H3 />
+          <RichTextEditor.H4 />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Blockquote />
+          <RichTextEditor.Hr />
+          <RichTextEditor.BulletList />
+          <RichTextEditor.OrderedList />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Link />
+          <RichTextEditor.Unlink />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.AlignLeft />
+          <RichTextEditor.AlignCenter />
+          <RichTextEditor.AlignJustify />
+          <RichTextEditor.AlignRight />
+        </RichTextEditor.ControlsGroup>
+      </RichTextEditor.Toolbar>
+
+      <RichTextEditor.Content />
+    </RichTextEditor>
+  );
+
   // editor created/destroyed by useEditor automatically
-  return <EditorContent editor={editor} />;
+  return <EditorContent editor={editor} className="editor" />;
 }
